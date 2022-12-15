fee4cedfb66c7c11b8a2f50d5439f710b245c03c
David Smith <96678397+Moomins07@users.noreply.github.com>
2022-12-15 13:55:15 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1020px",
+      xl: "1440px",
+    },
+    extend: {
+      backgroundImage: {
+        "bg-wallpaper": "url('/Images/gradient-blue.jpg')",
+        "casoftware-logo": "url('/Images/casoftware-fit.svg')",
+      },
+      colors: {
+        softBlue: "hsl(231, 69%, 60%)",
+        softRed: "hsl(0, 94%, 66%)",
+        grayishBlue: "hsl(229, 8%, 60%)",
+        veryDarkBlue: "hsl(229, 31%, 21%)",
+      },
+      fontFamily: {
+        sans: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
